90eb6f52585ff2d71f25ee9579e4161bd74b5e16
Panthawit Khantipan <panthawit.k@gmail.com>
2024-07-12 14:07:08 +0700

    add react router

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
 
+
 function App() {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
@@ -21,6 +22,7 @@ function App() {
   });
 
   return (
+    
     <div className="App">
       <h1>create-liff-app</h1>
       {message && <p>{message}</p>}
