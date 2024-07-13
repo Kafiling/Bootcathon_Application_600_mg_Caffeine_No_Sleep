b51e09861887ea5a44977d3788701da50b933c49
Sirichet Jomboonruang <jessyr@Jomboonruangs-Macbook-Air.local>
2024-07-13 14:14:04 +0700

    responsive-page

--- src/routes/CarCare.tsx
+++ src/routes/CarCare.tsx
@@ -2,96 +2,123 @@ import React from "react";
 
 export const CarCare = (): JSX.Element => {
     return (
-        <div className="bg-white flex flex-row justify-center w-full">
-            <div className="bg-white overflow-hidden w-[430px] h-[932px]">
-                <div className="relative w-[442px] h-[932px] -left-0.5">
-                    <div className="absolute w-[430px] h-[932px] top-0 left-0.5">
-                        <div className="absolute w-[430px] h-12 top-0 left-0 bg-[#d9d9d9]" />
-                        <div className="absolute w-[430px] h-[95px] top-[837px] left-0 bg-[#161616]">
-                            <img className="absolute w-[153px] h-[5px] top-20 left-[138px]" alt="Line" src="line-3.svg" />
+        <div className="bg-white flex flex-col items-center w-full min-h-screen">
+            <div className="w-full max-w-md h-auto">
+                <div className="relative w-full h-full">
+                    <div className="absolute w-full h-full top-0 left-0">
+                        <div className="absolute w-full h-12 top-0 left-0 bg-[#d9d9d9]" />
+                        <div className="absolute w-full h-[95px] top-[calc(100vh-95px)] left-0 bg-[#161616]">
+                            <img className="absolute w-[153px] h-[5px] top-20 left-[50%] transform -translate-x-1/2" alt="Line" src="line-3.svg" />
                         </div>
-                        <div className="absolute w-[430px] h-12 top-[58px] left-0 bg-[#e6e6e6]" />
-                        <div className="absolute w-[430px] h-[721px] top-[116px] left-0 bg-[#f1f1f1]" />
+                        <div className="absolute w-full h-12 top-[58px] left-0 bg-[#e6e6e6]" />
+                        <div className="absolute w-full h-[calc(100vh-183px)] top-[116px] left-0 bg-[#f1f1f1]" />
                     </div>
-                    <div className="absolute w-[442px] h-[635px] top-48 left-0">
-                        <div className="absolute w-[430px] h-12 top-0 left-0.5 bg-[#2e60a7]">
-                            <div className="w-[301px] top-[7px] left-[17px] text-white text-[28px] absolute [font-family:'Inter-Bold',Helvetica] font-bold tracking-[0] leading-[normal]">
+                    <div className="absolute w-full h-[635px] top-48 left-0">
+                        <div className="absolute w-full h-12 top-0 left-0 bg-[#2e60a7]">
+                            <div className="w-[301px] top-[7px] left-[17px] text-white text-[28px] absolute font-bold tracking-[0] leading-[normal]">
                                 คุณต้องการใช้บริการไหน
                             </div>
                         </div>
-                        <div className="absolute w-[432px] h-[587px] top-12 left-0">
-                            <div className="absolute w-[430px] h-[587px] top-0 left-0 bg-white" />
-                            <div className="absolute w-[379px] h-[544px] top-[19px] left-[26px]">
-                                <div className="absolute w-[378px] h-[368px] top-44 left-0">
-                                    <div className="absolute w-[177px] h-[177px] top-0 left-0 bg-neutral-50 rounded-[10px] shadow-[0px_4px_4px_#00000040]" />
-                                    <div className="absolute w-[177px] h-[177px] top-0 left-[201px] bg-neutral-50 rounded-[10px] shadow-[0px_4px_4px_#00000040]" />
-                                    <div className="absolute w-[177px] h-[177px] top-[191px] left-0 bg-neutral-50 rounded-[10px] shadow-[0px_4px_4px_#00000040]" />
-                                    <div className="absolute w-[177px] h-[177px] top-[191px] left-[201px] bg-neutral-50 rounded-[10px] shadow-[0px_4px_4px_#00000040]" />
+                        <div className="absolute w-full h-[587px] top-12 left-0">
+                            <div className="absolute w-full h-[587px] top-0 left-0 bg-white" />
+                            <div className="absolute w-[90%] max-w-[379px] h-[544px] top-[19px] left-1/2 transform -translate-x-1/2">
+                                <div className="absolute w-full h-[368px] top-44 left-0 grid grid-cols-2 gap-4">
+                                    <div className="w-full h-[177px] bg-neutral-50 rounded-[10px] shadow-md relative">
+                                        <img
+                                            className="w-full h-full object-cover absolute top-0 left-0"
+                                            alt="Image"
+                                            src="image-47.png"
+                                        />
+                                        <div className="text-black font-bold tracking-[0] leading-[normal] text-center text-sm xs:text-base absolute bottom-1.5 left-0 right-0">
+                                            การบำรุงรักษาทั่วไป
+                                        </div>
+                                    </div>
+                                    <div className="w-full h-[177px] bg-neutral-50 rounded-[10px] shadow-md relative">
+                                        <img
+                                            className="w-full h-full object-cover absolute top-0 left-0"
+                                            alt="Image"
+                                            src="image-48.png"
+                                        />
+                                        <div className="text-black font-bold tracking-[0] leading-[normal] text-center text-sm xs:text-base absolute bottom-1.5 left-0 right-0">
+                                            การบำรุงรักษาทั่วไป
+                                        </div>
+                                    </div>
+                                    <div className="w-full h-[177px] bg-neutral-50 rounded-[10px] shadow-md relative">
+                                        <img
+                                            className="w-full h-full object-cover absolute top-0 left-0"
+                                            alt="Image"
+                                            src="image-49.png"
+                                        />
+                                        <div className="text-black font-bold tracking-[0] leading-[normal] text-center text-sm xs:text-base absolute bottom-1.5 left-0 right-0">
+                                            การบำรุงรักษาทั่วไป
+                                        </div>
+                                    </div>
+                                    <div className="w-full h-[177px] bg-neutral-50 rounded-[10px] shadow-md relative">
+                                        <img
+                                            className="w-full h-full object-cover absolute top-0 left-0"
+                                            alt="Image"
+                                            src="image-50.png"
+                                        />
+                                        <div className="text-black font-bold tracking-[0] leading-[normal] text-center text-sm xs:text-base absolute bottom-1.5 left-0 right-0">
+                                            การบำรุงรักษาทั่วไป
+                                        </div>
+                                    </div>
                                 </div>
                                 <img
-                                    className="absolute w-[386px] h-[140px] -top-0.5 left-[-3px] object-cover"
+                                    className="absolute w-[386px] h-[140px] -top-0.5 left-1/2 transform -translate-x-1/2 object-cover"
                                     alt="Emergency syria"
                                     src="emergency-syria-t-rkiye-appeal-1.png"
                                 />
                             </div>
-                            <div className="top-[344px] left-[49px] text-black text-base whitespace-nowrap absolute [font-family:'Inter-Bold',Helvetica] font-bold tracking-[0] leading-[normal]">
-                                การบำรุงรักษาทั่วไป
+                            <div className="absolute w-full top-[209px] left-0 grid grid-cols-2 gap-4 px-4">
+                                <img
+                                    className="w-full h-[135px] object-cover"
+                                    alt="Image"
+                                    src="image-47.png"
+                                />
+                                <img
+                                    className="w-full h-[135px] object-cover"
+                                    alt="Image"
+                                    src="image-48.png"
+                                />
+                                <img
+                                    className="w-full h-[135px] object-cover"
+                                    alt="Image"
+                                    src="image.png"
+                                />
+                                <img
+                                    className="w-full h-[135px] object-cover"
+                                    alt="Image"
+                                    src="image-49.png"
+                                />
                             </div>
-                            <div className="w-[197px] top-[342px] left-[233px] text-black text-base absolute [font-family:'Inter-Bold',Helvetica] font-bold tracking-[0] leading-[normal]">
-                                การตรวจเช็คระบบเบรก
-                            </div>
-                            <div className="top-[535px] left-[33px] text-black text-base whitespace-nowrap absolute [font-family:'Inter-Bold',Helvetica] font-bold tracking-[0] leading-[normal]">
-                                การตรวจเช็คยางรถยนต์
-                            </div>
-                            <div className="top-[535px] left-[231px] text-black text-base whitespace-nowrap absolute [font-family:'Inter-Bold',Helvetica] font-bold tracking-[0] leading-[normal]">
-                                การบำรุงรักษาเครื่องยนต์
-                            </div>
-                            <img
-                                className="absolute w-[145px] h-[135px] top-[209px] left-[42px] object-cover"
-                                alt="Image"
-                                src="image-47.png"
-                            />
-                            <img
-                                className="absolute w-[145px] h-[135px] top-[209px] left-[243px] object-cover"
-                                alt="Image"
-                                src="image-48.png"
-                            />
-                            <img
-                                className="absolute w-[145px] h-[135px] top-[401px] left-[243px] object-cover"
-                                alt="Image"
-                                src="image.png"
-                            />
-                            <img
-                                className="absolute w-[145px] h-[135px] top-[401px] left-[42px] object-cover"
-                                alt="Image"
-                                src="image-49.png"
-                            />
-                            <img className="absolute w-[430px] h-1.5 top-[163px] left-0.5" alt="Line" src="line-8.svg" />
+                            <div className="absolute w-full h-1.5 top-[163px] left-0 bg-[#F1F1F1]" />
                         </div>
                     </div>
-                    <header className="absolute w-[432px] h-[62px] top-[116px] left-0.5 bg-transparent">
-                        <div className="relative w-[430px] h-[62px] bg-[#004894]">
-                            <img className="absolute w-[83px] h-[45px] top-2 left-[7px]" alt="Image" src="image-9.png" />
-                            <div className="absolute w-[247px] top-3 left-[94px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[28px] tracking-[0] leading-[normal]">
+                    <header className="absolute w-full h-[62px] top-[116px] left-0 bg-transparent">
+                        <div className="relative w-full h-[62px] bg-[#004894] flex items-center justify-between px-4">
+                            <img className="w-[83px] h-[45px]" alt="Image" src="image-9.png" />
+                            <div className="font-bold text-white text-[28px] tracking-[0] leading-[normal]">
                                 CAR CARE
                             </div>
-                            <div className="absolute w-[57px] h-[57px] top-0.5 left-[365px] bg-white rounded-[28.5px] border-[3px] border-solid border-[#11315e]">
-                                <img className="absolute w-[35px] h-[47px] top-0.5 left-2" alt="Account" src="account.png" />
+                            <div className="w-[57px] h-[57px] bg-white rounded-full border-[3px] border-solid border-[#11315e] flex items-center justify-center">
+                                <img className="w-[35px] h-[47px]" alt="Account" src="account.png" />
                             </div>
                         </div>
                     </header>
-                    <div className="absolute w-[409px] h-6 top-[70px] left-[9px]">
-                        <div className="absolute top-0.5 left-28 [font-family:'Inter-Regular',Helvetica] font-normal text-black text-base tracking-[0] leading-[normal] whitespace-nowrap">
+                    <div className="absolute w-full h-6 top-[70px] left-0 flex items-center justify-between px-4">
+                        <img className="w-6 h-6 object-cover" alt="Back" src="back-1.png" />
+                        <div className="font-normal text-black text-base tracking-[0] leading-[normal]">
                             Exxon-Mobil-AtHome.com
                         </div>
-                        <img className="absolute w-6 h-6 top-0 left-0 object-cover" alt="Back" src="back-1.png" />
-                        <img className="absolute w-5 h-5 top-[3px] left-[387px] object-cover" alt="Close" src="close-1.png" />
+                        <img className="w-5 h-5 object-cover" alt="Close" src="close-1.png" />
                     </div>
-                    <img className="absolute w-[430px] h-12 top-0 left-0.5" alt="Head" src="head.png" />
+                    <img className="absolute w-full h-12 top-0 left-0" alt="Head" src="head.png" />
                 </div>
             </div>
         </div>
     );
 };
 
-export default CarCare
+export default CarCare;
+
