e2769f0374006536a30f591c8d9ef7a0a4e73128
Tatar <tanapon9011@gmail.com>
2024-07-15 03:26:59 +0700

    working markers using JSON (style WIP)

--- src/routes/MobilLocations.tsx
+++ src/routes/MobilLocations.tsx
@@ -1,42 +1,107 @@
-import React, {useState, useRef} from 'react'
+import React, {FunctionComponent, useState, useRef} from 'react'
 import GoogleMapReact from 'google-map-react';
 import { useNavigate } from "react-router-dom";
-import useSupercluster from "use-supercluster";
-import useSwr from "swr";
 import "./output.css"
+import {setDefaults,fromLatLng,setKey,geocode,RequestType} from "react-geocode";
 
-const fetcher = (...args) => fetch(...args).then(response => response.json());
-
-const Marker = ({children}) => children;
 
 function MobilLocations() { 
+    setKey(import.meta.env.VITE_GOOGLE_MAPS_API_KEY); // Your API key here.
     const navigate = useNavigate();
     function handleClick() {
         navigate("/findnearest");
     }
 
-    const mapRef = useRef();
-    const [zoom, setZoom] = useState(8);
-    const [bounds, setBounds] = useState(null);
-
-    const url =
-    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2024-04";
-    const { data, error } = useSwr(url, { fetcher });
-    const shops = data && !error ? data.slice(0, 500) : []; //slice =  numbers of shops to display
-
-    
-
-    
     const defaultProps = {
         //Cord of Bangkok ,unused saved for future use
         center: {
-          lat: 52.6376,
-          lng: -1.135171
+          lat: 13.736717,
+          lng: 100.523186
         },
         zoom: 8
       };
+    
+    type AnyReactComponentProps = {
+    lat: number | undefined;
+    lng: number | undefined;
+    text: string;
+    };
+    //const AnyReactComponent: FunctionComponent<AnyReactComponentProps> = ({ lat, lng, text }) => <><button onClick={getAddress} type="button" className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 right-14 relative">Select Location</button><img src="https://mobil-at-home.s3.ap-southeast-1.amazonaws.com/location-pin.png" className="h-6 sm:h-9" alt="Flowbite Logo" /></>;
+    const [userLocation, setUserLocation] = useState<{ latitude: any, longitude: any }>({ latitude: 13.736717, longitude: 100.523186 });
+
+    const getUserLocation = () => {
+        // if geolocation is supported by the users browser
+        if (navigator.geolocation) {
+          // get the current users location
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              // save the geolocation coordinates in two variables
+              const { latitude, longitude } = position.coords;
+              // update the value of userlocation variable
+              setUserLocation({ latitude, longitude });
+            },
+            // if there was an error getting the users location
+            (error) => {
+              console.error('Error getting user location:', error);
+              setUserLocation({ latitude: 13.736717, longitude: 100.523186 })
+            }
+          );
+        }
+        // if geolocation is not supported by the users browser
+        else {
+          console.error('Geolocation is not supported by this browser.');
+          setUserLocation({ latitude: 13.736717, longitude: 100.523186 })
+        }
+      };
+    
+      // Get formatted address, city, state, country from latitude & longitude.
+    
+      //set interval to get user location
+      const getAddress = () =>{
+       if (userLocation) {
+          geocode(RequestType.LATLNG, `${userLocation.latitude},${userLocation.longitude}`, {
+            location_type: "ROOFTOP", // Override location type filter for this request.
+            enable_address_descriptor: true, // Include address descriptor in response.
+          })
+            .then(({ results }) => {
+              const address = results[0].formatted_address;
+              const { city, state, country } = results[0].address_components.reduce(
+                (acc, component) => {
+                  if (component.types.includes("locality"))
+                    acc.city = component.long_name;
+                  else if (component.types.includes("administrative_area_level_1"))
+                    acc.state = component.long_name;
+                  else if (component.types.includes("country"))
+                    acc.country = component.long_name;
+                  return acc;
+                },
+                {}
+              );
+              console.log(city, state, country);
+              console.log(address);
+            })
+            .catch(console.error);
+        }
+        else { console.log("No user location found") }
+      }
       
-    const [userLocation, setUserLocation] = useState<{ latitude: number ; longitude: number  } | null>(null);
+      const handleApiLoaded = (map, maps) => {
+        // use map and maps objects
+        map.setOptions({ gestureHandling: "greedy" });
+        setInterval(() => {
+          const center = map.getCenter()
+          let lat = center.lat(); 
+          let lag = center.lng();
+          console.log(lat);
+          setUserLocation({ latitude: lat, longitude: lag });
+          
+        }, 2000);
+      };
+      
+      
+     
+
+
 
     return (
         <>
@@ -58,17 +123,13 @@ function MobilLocations() {
                 <div style={{ height: '80vh', width: '100%' }}>
                 
                 <GoogleMapReact
-                    bootstrapURLKeys={{ key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY}} /*TODO : Add Google Map API Key to .env file 8*/
-                    defaultCenter={userLocation ? { lat: userLocation.latitude, lng: userLocation.longitude } : { lat: 52.6376, lng: -1.135171 }}
+                    yesIWantToUseGoogleMapApiInternals = {true}
+                    bootstrapURLKeys={{ key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY }}
+                    defaultCenter={userLocation ? { lat: userLocation.latitude, lng: userLocation.longitude } : { lat: defaultProps.center.lat, lng: defaultProps.center.lng }}
                     defaultZoom={defaultProps.zoom}
+                    onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
                 >
-                    {shops.map(shop => (
-                    <Marker key={shop.id} lat={shop.location.latitude} lng={shop.location.longitude} >
-                        <button className="bg-none border-none">
-                        <img className = "h-6 w-6"src="../images/custody.svg" alt="Crime Icon" />
-                        </button>
-                    </Marker>
-                    ))}
+        
                 </GoogleMapReact>
                 </div>
 
