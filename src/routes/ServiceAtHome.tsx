16a31ccbd3dcf30674a03c19aa7cf0f1707287a8
Panthawit Khantipan <panthawit.k@gmail.com>
2024-07-14 15:35:53 +0700

    Before change to new package

--- src/routes/ServiceAtHome.tsx
+++ src/routes/ServiceAtHome.tsx
@@ -1,9 +1,15 @@
 import React, {FunctionComponent,useState} from 'react'
 import GoogleMapReact from 'google-map-react';
+import {setDefaults,fromLatLng,setKey,geocode,RequestType} from "react-geocode";
+
+// Set default response language and region (optional).
+// This sets default values for language and region for geocoding requests.
 
 
 import "./output.css"
 function ServiceAtHome() {
+  setKey("AIzaSyC2qNQnFlkRociA3pHan0w7uT_glPYUnLo"); // Your API key here.
+  
     const defaultProps = {
         //Cord of Bankkok ,unused saved for future use
         center: {
@@ -18,8 +24,8 @@ function ServiceAtHome() {
         text: string;
       };
       
-      const AnyReactComponent: FunctionComponent<AnyReactComponentProps> = ({ lat, lng, text }) => <img src="https://mobil-at-home.s3.ap-southeast-1.amazonaws.com/location-pin.png" className="h-6 sm:h-9" alt="Flowbite Logo" />;
-      const [userLocation, setUserLocation] = useState<{ latitude: number ; longitude: number  } | null>(null);
+      const AnyReactComponent: FunctionComponent<AnyReactComponentProps> = ({ lat, lng, text }) => <><button type="button" className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Red</button><img src="https://mobil-at-home.s3.ap-southeast-1.amazonaws.com/location-pin.png" className="h-6 sm:h-9" alt="Flowbite Logo" /></>;
+      const [userLocation, setUserLocation] = useState<{ latitude: number, longitude: number }>({ latitude: 13.736717, longitude: 100.523186 });
 
       // define the function that finds the users geolocation
   const getUserLocation = () => {
@@ -47,6 +53,43 @@ function ServiceAtHome() {
     }
   };
 
+  // Get formatted address, city, state, country from latitude & longitude.
+
+  //set interval to get user location
+  const getAddress = () =>{
+   if (userLocation) {
+      geocode(RequestType.LATLNG, `${userLocation.latitude},${userLocation.longitude}`, {
+        location_type: "ROOFTOP", // Override location type filter for this request.
+        enable_address_descriptor: true, // Include address descriptor in response.
+      })
+        .then(({ results }) => {
+          const address = results[0].formatted_address;
+          const { city, state, country } = results[0].address_components.reduce(
+            (acc, component) => {
+              if (component.types.includes("locality"))
+                acc.city = component.long_name;
+              else if (component.types.includes("administrative_area_level_1"))
+                acc.state = component.long_name;
+              else if (component.types.includes("country"))
+                acc.country = component.long_name;
+              return acc;
+            },
+            {}
+          );
+          console.log(city, state, country);
+          console.log(address);
+        })
+        .catch(console.error);
+    }
+    else { console.log("No user location found") }
+  }
+
+  
+  
+
+  
+ 
+
   return (
     <>
     <header>
@@ -64,7 +107,7 @@ function ServiceAtHome() {
 <section>
     <div className='bg-red-500'>Service-at-Home</div>
     <div style={{ height: '30vh', width: '100%' }}>
-    {/*TODO : Add Google Map API Key to .env file 8*/}
+    {/*TODO : Add Google Map API Key to .env file */}
       <GoogleMapReact
         bootstrapURLKeys={{ key: "" }}
         defaultCenter={userLocation ? { lat: userLocation.latitude, lng: userLocation.longitude } : { lat: 13.736717, lng: 100.523186 }}
